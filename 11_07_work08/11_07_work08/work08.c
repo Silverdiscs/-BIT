a07180cae1a7d1a13da5a279b170c77e5b07d2f8
Silverdiscs <silverdiscs521@gmail.com>
2018-11-07 15:45:33 +0800

    11月7日提交作业

---
+++ 11_07_work08/11_07_work08/work08.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main(){
+	int i = 0;
+	int k = 0;
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 10 == 9)
+		{
+			k++;
+		}
+		if (i / 10 == 9)
+		{
+			k++;
+		}
+	}
+	printf("%d\n", k);
+	return 0;
+}
