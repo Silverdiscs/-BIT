d7718780e5aa7af4cc7e0548ed7ade209bfa61e2
Silverdiscs <silverdiscs521@gmail.com>
2018-11-07 15:41:03 +0800

    11月7日提交作业

---
+++ 11_07_work01/11_07_work01/work01.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+
+	int tmp = a;
+	a = b;
+	b = tmp;
+
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	return 0;
+
+}
