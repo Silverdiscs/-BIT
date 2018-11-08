34719c35aa27acc16351aaa5f3debea2abf589f2
Silverdiscs <silverdiscs521@gmail.com>
2018-11-08 22:29:43 +0800

    2018_11_08_Code

---
+++ 11_08_DIY/11_08_DIY/11_08_diy.c
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//     The First Code Today——
+
+//int main()
+//{
+//	int input = 0;
+//	printf("你的老婆美不美？\n");
+//	scanf("%d", &input);
+//	if (input == 1)
+//	{
+//		printf("美美美，就是喜欢看流星花园\n");
+//	}
+//	else
+//	{
+//		printf("不美，等着回家挨揍吧\n");
+//	}
+//	return 0;
+//}
+
+
+
+//    2nd Code——
+//int main()
+//{
+//	int input = 0;
+//	printf("今天什么天气？\n");
+//	scanf("%d", &input);
+//	if (input == 1)
+//	{
+//		printf("晴天\n");
+//	}
+//	else
+//	{
+//		printf("阴天\n");
+//	}
+//	
+//
+//	return 0;
+//}
+
+
+//3rd Code——循环 while循环
+
+//int main()
+//{
+//	int line = 0;
+//
+//	while (line <= 20000)
+//	{
+//		printf("敲代码\n");
+//		line++;
+//	}
+//	if (line > 20000);
+//	{
+//		printf("大牛！！！\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//4th Code——
+//
+//int main()
+//{                                //一个选择的问题——满足这个条件怎么样，不满足又如何？
+//	
+//	int line = 0;                 //条件初始化，赋值
+//	
+//	while (line <= 10000)    //不满足这个条件
+//	{
+//		printf("敲代码\n");        //不满足这个条件的情况
+//		line++;                  //然后去——做
+//	}
+//	if (line > 10000)                //满足了这个条件，然后去...
+//	{
+//		printf("厉害了！\n");            //然后去——做
+//	}
+//	return 0;
+//}
+
+
+//5th Code——
+
+int main()
+{
+
+
+
+
+
+	return 0;
+}
+
+
