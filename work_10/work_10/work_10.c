c7b2e763b13d106e4af9b37bdf4308e41cd05fd8
Silverdiscs <silverdiscs521@gmail.com>
2018-11-24 21:46:33 +0800

    work10

---
+++ work_10/work_10/work_10.c
@@ -0,0 +1,168 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//写一个函数返回参数二进制中 1 的个数
+
+//int count_one_bits(unsigned int num)
+//{
+//	int count = 0;
+//	while (num)
+//	{
+//		num = num & (num - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	printf("请输入数字：\n");
+//	scanf("%d", &n);
+//	int ret = count_one_bits(n);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
